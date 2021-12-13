151d9c973428745483b08dc49c5e87606bf3108c
oliver <olivermarco97@gmail.com>
2021-12-13 09:27:41 +0100

    footer.html oprettet

---
+++ includes/footer.php
@@ -0,0 +1,76 @@
+<footer class="page-footer pt-5 pb-3">
+
+    <div class="container footerMargin">
+        <div class="row">
+
+            <div class="col-xs-12 col-sm-6 col-lg-3">
+                <div class="footerMenu">
+                    <div class="productBox">
+                        <h2>Produkter</h2>
+                        <a href="#">Desktop</a>
+                        <a href="#">skærme</a>
+                        <a href="#">tilbehør</a>
+                        <a href="#">support</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-lg-3 ">
+                <div class="footerSupport">
+                    <div class="supportBox">
+                        <h2>support</h2>
+                        <a href="#">kontakt</a>
+                        <a href="#">om os</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-lg-6 ">
+                <div class="footerFollow">
+                    <div class="followBox">
+                        <h2 class="mb-3">fang os på</h2>
+                        <div class="some mb-5">
+                            <a class="icons" target="_blank" href="#">
+                                <i class="fab fa-facebook-square"></i>
+                            </a>
+                            <a class="icons" target="_blank" href="#">
+                                <i class="fab fa-facebook-square"></i>
+                            </a>
+                            <a class="icons" target="_blank" href="#">
+                                <i class="fab fa-facebook-square"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footerLine">
+
+    </div>
+
+
+
+    <div class="container">
+
+
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <div class="copyright">
+                    <p>Copyrigt 2021. Alle rettigheder forbeholdes</p>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="danmark">
+                    <p class="text-uppercase">Danmark</p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</footer>
+
