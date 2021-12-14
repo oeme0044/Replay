d2ca58dfd163757addb436bf3a6eea65efa0d41b
omertekbas <tekbas425@gmail.com>
2021-12-14 09:59:52 +0100

    navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,32 +1,31 @@
-<nav class="navbar navbar-expand-lg navbar-dark">
+<nav class="navbar navbar-expand-lg">
     <div class="navcon container-fluid">
         <a class="navbar-brand" href="#">
-            <img src="./Image/logo.png" alt="" width="100%" height="35">
+            <img src="./Image/Replay%20Logo%20sort.svg" alt="" width="100%" height="30">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
+                    <a class="nav-link text-uppercase" href="#">desktops</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">About</a>
+                    <a class="nav-link text-uppercase" href="#">skærme</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Portfolio</a>
+                    <a class="nav-link text-uppercase" href="#">tilbehør</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Skills</a>
+                    <a class="nav-link text-uppercase" href="#">suppprt</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
-                </li>
-            </ul>
 
+            </ul>
+        </div>
+        <div class="nav-item me-3 me-lg-0">
+            <a class="kurv" href="#"><i class="fas fa-shopping-bag"></i></a>
         </div>
 
     </div>
-
 </nav>
